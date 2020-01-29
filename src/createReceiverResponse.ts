7bf7447b320f3c228974a263cb0c93dc0d21544f
ivyhjk <elvis.munoz.f@gmail.com>
2020-01-29 10:15:22 -0300

    Add POST /receivers response

---
+++ src/createReceiverResponse.ts
@@ -0,0 +1,13 @@
+interface CreateReceiverResponse {
+  /**
+   * Identificador único de la cuenta de cobro.
+   */
+  receiver_id: string;
+  /**
+   * Llave secreta de la cuenta de cobro, se usa para firmar todas las
+   * peticiones.
+   */
+  secret: string;
+}
+
+export default CreateReceiverResponse;
