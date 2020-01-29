242a6b7c36efa4871d4483657dad5151a62534b9
ivyhjk <elvis.munoz.f@gmail.com>
2020-01-29 10:16:16 -0300

    Add main file

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import BanksResponse from './banksResponse';
+import Client from './client';
+import Configuration from './configuration';
+import ConfirmPaymentResponse from './confirmPaymentResponse';
+import CreatePaymentRequest from './createPaymentRequest';
+import CreatePaymentResponse from './createPaymentResponse';
+import CreateReceiverRequest from './createReceiverRequest';
+import CreateReceiverResponse from './createReceiverResponse';
+import DeletePaymentResponse from './deletePaymentResponse';
+import PaymentResponse from './paymentResponse';
+import PaymentsResponse from './paymentsResponse';
+import RefundPaymentResponse from './refundPaymentResponse';
+import Request from './request';
+import SuccessResponse from './successResponse';
+
+export {
+  BanksResponse,
+  Client,
+  Configuration,
+  ConfirmPaymentResponse,
+  CreatePaymentRequest,
+  CreatePaymentResponse,
+  CreateReceiverRequest,
+  CreateReceiverResponse,
+  DeletePaymentResponse,
+  PaymentResponse,
+  PaymentsResponse,
+  RefundPaymentResponse,
+  Request,
+  SuccessResponse
+};
