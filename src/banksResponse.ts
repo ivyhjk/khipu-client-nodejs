ac188387680ffc4c5f99c61f445f16018502dcb1
ivyhjk <elvis.munoz.f@gmail.com>
2020-01-29 10:11:42 -0300

    Add GET /banks response

---
+++ src/banksResponse.ts
@@ -0,0 +1,33 @@
+interface Bank {
+  /**
+   * Identificador del banco.
+   */
+  bank_id: string;
+  /**
+   * Nombre del banco.
+   */
+  name: string;
+  /**
+   * Mensaje con particularidades del banco.
+   */
+  message: string;
+  /**
+   * Monto mínimo que acepta el banco en un pago.
+   */
+  min_amount: number;
+  /**
+   * Tipo de banco.
+   */
+  type: string;
+  /**
+   * Identificador del banco padre (si un banco tiene banca personas y
+   * empresas, el primero será el padre del segundo).
+   */
+  parent: string;
+}
+
+interface BankResponse {
+  banks: ReadonlyArray<Bank>
+}
+
+export default BankResponse;
