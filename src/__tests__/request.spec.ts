269263f8deb433affd99622009b918b303acddbf
ivyhjk <elvis.munoz.f@gmail.com>
2020-01-29 10:59:13 -0300

    Add main request

---
+++ src/__tests__/request.spec.ts
@@ -0,0 +1,153 @@
+import crypto from 'crypto';
+
+import Request from './../request';
+
+describe('khipu-client.request', () => {
+  it('test given configurations', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'GET',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    expect(request.configuration.endpoint).toBe('/foo');
+    expect(request.configuration.method).toBe('GET');
+    expect(request.configuration.receiverId).toBe('a-receiver-id');
+    expect(request.configuration.secret).toBe('a-secret');
+  });
+
+  it('should check a valid endpoint', async () => {
+    const first = new Request({
+      endpoint: 'foo',
+      method: 'GET',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    expect(first.getEndpoint()).toBe('/foo');
+
+    const second = new Request({
+      endpoint: '/bar',
+      method: 'GET',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    expect(second.getEndpoint()).toBe('/bar');
+  });
+
+  it('should check a valid URL path', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'GET',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    expect(request.getPath()).toBe(`${Request.API_ENDPOINT}/foo`);
+  });
+
+  it('should check a valid url', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'GET',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    const expected = `https://${Request.HOST_NAME}${Request.API_ENDPOINT}/foo`;
+
+    expect(request.getUrl()).toBe(expected);
+  });
+
+  it('should check a valid hash', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'POST',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret',
+      body: {
+        foo: 'bar',
+        bar: 'baz',
+        baz: 'foo'
+      }
+    });
+
+    const encodedURL = encodeURIComponent(request.getUrl());
+
+    const data = `POST&${encodedURL}&foo=bar&bar=baz&baz=foo`;
+
+    const expected = crypto
+      .createHmac('sha256', 'a-secret')
+      .update(data)
+      .digest('hex');
+
+    expect(request.getHash()).toBe(expected);
+  });
+
+  it('should check a valid post data', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'POST',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret',
+      body: {
+        foo: 'bar',
+        bar: 'baz',
+        baz: 'foo'
+      }
+    });
+
+    expect(request.getPostData()).toBe('foo=bar&bar=baz&baz=foo');
+  });
+
+  it('should validate the request options without a body', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'DELETE',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret'
+    });
+
+    expect(request.getOptions().hostname).toBe(Request.HOST_NAME);
+    expect(request.getOptions().port).toBe(443);
+    expect(request.getOptions().path).toBe(request.getPath());
+    expect(request.getOptions().method).toBe('DELETE');
+    expect(request.getOptions().headers!.Authorization).toBe(
+      `a-receiver-id:${request.getHash()}`
+    );
+    expect(request.getOptions().headers!['Content-Type']).toBe(
+      'application/x-www-form-urlencoded'
+    );
+    expect(request.getOptions().headers!['Content-Length']).toBeUndefined();
+  });
+
+  it('should validate the request options with a body', async () => {
+    const request = new Request({
+      endpoint: '/foo',
+      method: 'DELETE',
+      receiverId: 'a-receiver-id',
+      secret: 'a-secret',
+      body: {
+        foo: 'bar',
+        bar: 'baz',
+        baz: 'foo'
+      }
+    });
+
+    expect(request.getOptions().hostname).toBe(Request.HOST_NAME);
+    expect(request.getOptions().port).toBe(443);
+    expect(request.getOptions().path).toBe(request.getPath());
+    expect(request.getOptions().method).toBe('DELETE');
+    expect(request.getOptions().headers!.Authorization).toBe(
+      `a-receiver-id:${request.getHash()}`
+    );
+    expect(request.getOptions().headers!['Content-Type']).toBe(
+      'application/x-www-form-urlencoded'
+    );
+    expect(request.getOptions().headers!['Content-Length']).toBe(
+      request.getPostData()!.length
+    );
+  });
+});
---
+++ src/request.ts
@@ -0,0 +1,168 @@
+import crypto from 'crypto';
+import https from 'https';
+import querystring from 'querystring';
+
+import AuthorizationError from './errors/authorizationError';
+import ValidationError from './errors/validationError';
+
+export type Method = 'GET' | 'POST' | 'DELETE';
+
+interface RequestConfiguration<T> {
+  endpoint: string;
+  method: Method;
+  secret: string;
+  receiverId: string;
+  body?: T;
+}
+
+class Request<TResponse, TRequest = any> {
+  public static readonly HOST_NAME = 'khipu.com';
+  public static readonly API_ENDPOINT = '/api/2.0';
+  private url?: string;
+  private endpoint?: string;
+  private path?: string;
+  private hash?: string;
+  private options?: https.RequestOptions;
+  private postData?: null | string;
+
+  public constructor(
+    public readonly configuration: RequestConfiguration<TRequest>
+  ) {
+
+  }
+
+  public getEndpoint(): string {
+    if (!this.endpoint) {
+      this.endpoint = '/' + this.configuration.endpoint.replace(/^\//, '');
+    }
+
+    return this.endpoint;
+  }
+
+  public getPath(): string {
+    if (!this.path) {
+      this.path = Request.API_ENDPOINT + this.getEndpoint();
+    }
+
+    return this.path;
+  }
+
+  public getUrl(): string {
+    if (!this.url) {
+      this.url = `https://${Request.HOST_NAME}`;
+      this.url += this.getPath();
+    }
+
+    return this.url;
+  }
+
+  public getHash(): string {
+    if (!this.hash) {
+      const chunks = [];
+
+      chunks.push(this.configuration.method);
+      chunks.push(encodeURIComponent(this.getUrl()));
+
+      if (this.configuration.body) {
+        const body: { [key: string]: any } = this.configuration.body;
+        const keys = Object.keys(body);
+
+        for (const key of keys) {
+          const value = body[key];
+          const encodedKey = encodeURIComponent(key);
+          const encodedValue = encodeURIComponent(value);
+
+          chunks.push(`${encodedKey}=${encodedValue}`);
+        }
+      }
+
+      // @see https://khipu.com/page/api-firma-requerimientos
+      this.hash = crypto
+        .createHmac('sha256', this.configuration.secret)
+        .update(chunks.join('&'))
+        .digest('hex');
+    }
+
+    return this.hash;
+  }
+
+  public getPostData(): null | string {
+    if (typeof this.postData === 'undefined') {
+      if (!this.configuration.body) {
+        this.postData = null;
+      } else {
+        // @ts-ignore
+        this.postData = querystring.stringify(this.configuration.body);
+      }
+    }
+
+    return this.postData;
+  }
+
+  public getOptions(): https.RequestOptions {
+    if (!this.options) {
+      let headers: { [key: string]: any } = {
+        Authorization: `${this.configuration.receiverId}:${this.getHash()}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      };
+
+      const postData = this.getPostData();
+
+      if (postData) {
+        headers['Content-Length'] = postData.length;
+      }
+
+      this.options = {
+        hostname: Request.HOST_NAME,
+        port: 443,
+        path: this.getPath(),
+        method: this.configuration.method,
+        headers
+      };
+    }
+
+    return this.options;
+  }
+
+  public async execute(): Promise<TResponse> {
+    return new Promise((resolve, reject) => {
+      const req = https.request(this.getOptions(), (res) => {
+        let chunks: string = '';
+
+        res.on('data', (chunk) => {
+          chunks += chunk;
+        });
+
+        res.on('end', () => {
+          try {
+            const response = JSON.parse(chunks);
+
+            if (res.statusCode === 403) {
+              reject(new AuthorizationError());
+            } else if (res.statusCode === 400) {
+              reject(new ValidationError(response?.errors[0]?.message));
+            } else {
+              resolve(response);
+            }
+          } catch (e) {
+            reject(e);
+          }
+        });
+      });
+
+      req.on('error', (e) => {
+        reject(e);
+      });
+
+      const postData = this.getPostData();
+
+      if (postData) {
+        req.write(postData);
+      }
+
+      req.end();
+    });
+  }
+}
+
+export default Request;
